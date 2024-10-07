bd3aa98861d6534c81b2c3015d1116058a5fd1a3
AnkitM2001 <ankit31.mishra2001@gmail.com>
2024-10-07 14:19:12 +0530

    new

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -55,7 +55,32 @@ export async function POST(req: Request) {
   const eventType = evt.type;
  
   if(eventType === 'user.created') {
-    const { id,  image_url, first_name, last_name, username } = evt.data;
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name!,
+      lastName: last_name!,
+      photo: image_url,
+    }
+
+    const newUser = await createUser(user);
+
+    if(newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id
+        }
+      })
+    }
+
+    return NextResponse.json({ message: 'OK', user: newUser })
+  }
+
+  if (eventType === 'user.updated') {
+    const {id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
       firstName: first_name!,
@@ -65,14 +90,18 @@ export async function POST(req: Request) {
     }
 
     const updatedUser = await updateUser(id, user)
-    return NextResponse.json({message: 'OK' , user: updateUser})
+
+    return NextResponse.json({ message: 'OK', user: updatedUser })
   }
-  if (eventType === 'user.deleted'){
+
+  if (eventType === 'user.deleted') {
     const { id } = evt.data
 
     const deletedUser = await deleteUser(id!)
 
-    return NextResponse.json({message: 'OK' , user: deleteUser })
+    return NextResponse.json({ message: 'OK', user: deletedUser })
   }
-  return new Response('',{ status: 200 })
-  }
+ 
+  return new Response('', { status: 200 })
+}
+ 
